934aa27f3f11a4d8a9d0cb1f7b574cec448105ee
cquest <cquest@openstreetmap.fr>
2018-02-11 19:15:19 +0100

    python packages update

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 cd /home/pi/pynitel
+
+# get the latest version available
 /usr/bin/git pull
+
+# update requirements if needed
+/usr/bin/pip3 install -r requirements.txt --upgrade
+
+# start the main python script
 /usr/bin/python3 example_annuaire.py
