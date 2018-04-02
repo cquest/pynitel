921e1cb3a417b124ca5da2791f2d71ea4c97d353
Christian Quest <cquest@openstreetmap.fr>
2018-04-02 18:48:38 +0200

    gestion code de service dans le path du websocket

--- 3615.py
+++ 3615.py
@@ -37,13 +37,17 @@ async def teletel_saisie(m, codes):
 
 async def teletel(websocket, path):
     m = pynitel.Pynitel(pynitel.PynitelWS(websocket))
+    # on récupère le code du service depuis le path du webSocket
+    # //ws -> vide  /ulla/ws -> ULLA etc...
+    code = path.replace('/ws', '').replace('/', '').upper().strip()
+    touche = m.envoi
+    # liste des service connus...
+    codes = ['11', '3611', 'AE', 'ANNU', 'ANNUAIRE',
+             'ULLA']
+
     while True:
-        codes = ['11', '3611', 'AE', 'ANNU', 'ANNUAIRE',
-                 'ULLA']
         await m._print(m.PRO2+'\x6A\x45')  # passage clavier standard
-        (touche, code) = await teletel_saisie(m, codes)
-        if touche == m.envoi:
-            code = code.upper().strip()
+        if code != '':
             # on lance la recherche
             await m.pos(0, 1)
             await m.flash()
@@ -54,6 +58,12 @@ async def teletel(websocket, path):
                 await annuaire_teletel(m)
             elif code == 'ULLA':
                 await ulla_teletel(m)
+
+        (touche, code) = await teletel_saisie(m, codes)
+        if touche == m.envoi:
+            code = code.upper().strip()
+        else:
+            code = ''
 
 
 if __name__ == '__main__':
