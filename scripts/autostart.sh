20dc4772663bcf5426456718a1aa7445c11b246b
Christian Quest <cquest@openstreetmap.fr>
2018-08-13 16:33:25 +0200

    autostart FUG

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,7 +2,7 @@
 cd /home/pi/pynitel
 
 # wait IP connection (ping to github)
-for i in {1..50}; do ping -c1 www.118712.fr &> /dev/null && break; done
+#for i in {1..50}; do ping -c1 www.118712.fr &> /dev/null && break; done
 
 # start the main python script
-/usr/bin/python3 example_annuaire.py
+/usr/bin/python3 fug.py
